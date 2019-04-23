678d519cf979fa450c2eec1833b2c8c181cfce02
Josh Grillo <Josh@Grillo.co.uk>
2019-04-23 12:51:38 +0100

    Made the IOHandler do some things

--- Blocks_Game/Blocks_Game/IOHandler.cpp
+++ Blocks_Game/Blocks_Game/IOHandler.cpp
@@ -4,9 +4,36 @@
 
 IOHandler::IOHandler()
 {
+	std::vector <serial::PortInfo> devices = serial::list_ports();
+	std::vector <serial::PortInfo>::iterator iter = devices.begin();
+
+	while (iter != devices.end())
+	{
+		serial::PortInfo device = *iter++;
+		std::string port = device.port.c_str();
+
+		try 
+		{
+			mySerial = new serial::Serial(port, 9600, serial::Timeout::simpleTimeout(250));
+
+			if (mySerial->isOpen())
+			{
+				// std::cout << "Connection success: " << port << std::endl
+				connect = true;
+				break;
+			}
+		}
+		catch (std::exception e) {} // this line might break everything, i hope it doesn't
+	}
 }
 
 
 IOHandler::~IOHandler()
 {
+}
+
+void IOHandler::close()
+{
+	mySerial->flush();
+	mySerial->close();
 }
--- Blocks_Game/Blocks_Game/IOHandler.h
+++ Blocks_Game/Blocks_Game/IOHandler.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "serial/serial.h"
+#include <string>
+#include <vector>
+#include <iterator>
+
 class IOHandler
 {
 public:
 	IOHandler();
 	~IOHandler();
+
+	bool connect = false;
+
+	void close();
+
+private:
+	serial::Serial* mySerial;
 };
 
